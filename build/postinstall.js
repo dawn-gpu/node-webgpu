c4d79aefbd917232e8e860a498c9f9ed662d90c2
Gregg Tavares <gman@chromium.org>
2025-09-23 09:50:28 -0700

    fix attribute check

--- build/postinstall.js
+++ build/postinstall.js
@@ -1,11 +1,15 @@
-import path from 'node:path';
-import fs from 'node:fs';
+import child_process from 'node:child_process';
 
 import {execute} from './execute.js';
 import {isMac} from './constants.js';
 
 async function main() {
   if (isMac) {
+    const result = child_process.execFileSync('xattr', ['-l', 'dist/darwin-universal.dawn.node']);
+    if (!result.toString().includes('com.apple.quarantine')) {
+      return;
+    }
+
     // The user has already indicated they trust this by installing it,
     // This executable can not do anything a JavaScript node script couldn't also do.
     await execute('xattr', ['-d', 'com.apple.quarantine', 'dist/darwin-universal.dawn.node']);
